9aa4d852958277fdf2910a2a504383c7efb60caf
sid420 <sawan.nepal@selise.ch>
2022-10-11 16:28:08 +0600

    chore: refactor code

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: %i[show index new create]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
 end
